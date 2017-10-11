74905fb7e276f859dcd6dbc50584d07fb0a0c843
Marcel Fraas <mfraas64@gmail.com>
2017-10-11 15:04:33 +0200

    Webcam Added

--- compose_webcam/images/ffmpeg/entrypoint.sh
+++ compose_webcam/images/ffmpeg/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ffmpeg -f mjpeg -i "http://webcam:fsmpi@olaf.fs.uni-bayreuth.de/video.cgi" -f flv rtmp://rtmpserver/live
+#ffmpeg -f mjpeg -i "http://webcam:fsmpi@olaf.fs.uni-bayreuth.de/video.cgi" -f flv rtmp://rtmpserver/live
+
+ffmpeg -f mjpeg -i "http://webcam:fsmpi@olaf.fs.uni-bayreuth.de/video.cgi" -vcodec libx264 -acodec aac -f flv rtmp://rtmpserver/live
